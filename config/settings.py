81140b2f103e8c385bd5e6b45693e8f6112d249a
Yuretzz <yuvede@gmail.com>
2016-10-18 17:08:35 +0300

    autocss concept

--- config/settings.py
+++ config/settings.py
@@ -128,5 +128,6 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "media")
+    os.path.join(BASE_DIR, "media"),
+    os.path.join(BASE_DIR, "scripts"),
 ]
---
+++ scripts/dynamiclayout.js
@@ -0,0 +1,75 @@
+// - - - - - - - - - - - - - - - - - - - - -
+//
+// Title : Dynamic Resolution Dependent Layout Demo
+// Author : Kevin Hale
+// URL : http://particletree.com
+//
+// Description : This is a demonstration of a dynamic 
+// resolution dependent layout in action. Change your browser 
+// window size to see the layout respond to your changes. To 
+// preserve the separation of the presentation and behavior 
+// layers, this implementation delegates all the presentation 
+// details to external CSS stylesheets instead of changing 
+// each style property through JavaScript.
+//
+// Created : July 30, 2005
+// Modified : November 15, 2005
+//
+// - - - - - - - - - - - - - - - - - - - - -
+
+// getBrowserWidth is taken from The Man in Blue Resolution Dependent Layout Script
+// http://www.themaninblue.com/experiment/ResolutionLayout/
+	function getBrowserWidth(){
+		if (window.innerWidth){
+			return window.innerWidth;}	
+		else if (document.documentElement && document.documentElement.clientWidth != 0){
+			return document.documentElement.clientWidth;	}
+		else if (document.body){return document.body.clientWidth;}		
+			return 0;
+	}
+
+// dynamicLayout by Kevin Hale
+function dynamicLayout(){
+	var browserWidth = getBrowserWidth();
+// alert(browserWidth);
+	//Load Thin CSS Rules
+	if (browserWidth < 750){
+		changeLayout("thin");
+	}
+	//Load Wide CSS Rules
+	if ((browserWidth >= 1024) && (browserWidth < 1600)){
+		changeLayout("wide");
+		// alert("between 1024 and less than 1600")
+	}
+	//Load Wider CSS Rules
+	if (browserWidth >= 1600){
+		changeLayout("wider");
+		// alert("greater or equal to 1600")
+	}
+}
+
+// changeLayout is based on setActiveStyleSheet function by Paul Sowdon 
+// http://www.alistapart.com/articles/alternate/
+function changeLayout(description){
+   var i, a;
+   for(i=0; (a = document.getElementsByTagName("link")[i]); i++){
+	   if(a.getAttribute("title") == description){a.disabled = false;}
+	   else if(a.getAttribute("title") != "default"){a.disabled = true;}
+   }
+}
+
+	//addEvent() by John Resig
+	function addEvent( obj, type, fn ){ 
+	   if (obj.addEventListener){ 
+	      obj.addEventListener( type, fn, false );
+	   }
+	   else if (obj.attachEvent){ 
+	      obj["e"+type+fn] = fn; 
+	      obj[type+fn] = function(){ obj["e"+type+fn]( window.event ); } 
+	      obj.attachEvent( "on"+type, obj[type+fn] ); 
+	   } 
+	} 
+	
+	//Run dynamicLayout function when page loads and when it resizes.
+	addEvent(window, 'load', dynamicLayout);
+	addEvent(window, 'resize', dynamicLayout);
