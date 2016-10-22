2b1e1b6834e28185008a5aed2ded2030bb33cb62
Yuretzz <yuvede@gmail.com>
2016-10-22 11:40:36 +0300

    merged

--- config/settings.py
+++ config/settings.py
@@ -16,8 +16,8 @@ import locale
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 ENC = locale.getpreferredencoding()
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))).decode(ENC)
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))).decode(ENC)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
